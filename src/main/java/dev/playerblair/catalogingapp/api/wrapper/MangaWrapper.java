9561642082f8c256d55ae09a18c259146f28af5b
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-02-28 04:43:17 +0000

    setup MangaWrapper

--- src/main/java/dev/playerblair/catalogingapp/api/wrapper/MangaWrapper.java
+++ src/main/java/dev/playerblair/catalogingapp/api/wrapper/MangaWrapper.java
@@ -1,4 +1,26 @@
 package dev.playerblair.catalogingapp.api.wrapper;
 
+import dev.playerblair.catalogingapp.manga.model.Author;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MangaWrapper {
+
+    private Long malId;
+    private String title;
+    private String type;
+    private int chapters;
+    private int volumes;
+    private String status;
+    private List<Author> authors;
+    private List<GenreWrapper> genres;
+    private String url;
 }
