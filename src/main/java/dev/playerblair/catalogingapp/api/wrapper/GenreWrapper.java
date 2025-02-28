e4e0a978c3a58e737b5aa4515a302e568caae5ff
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-02-28 04:44:34 +0000

    add GenreWrapper

---
+++ src/main/java/dev/playerblair/catalogingapp/api/wrapper/GenreWrapper.java
@@ -0,0 +1,11 @@
+package dev.playerblair.catalogingapp.api.wrapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GenreWrapper {
+
+    private String name;
+}
