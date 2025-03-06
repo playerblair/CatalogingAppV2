57c8c613a2b780b265fe5cb30377b7058567dca6
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-03-06 23:01:01 +0000

    remove unused imports and change name of test objects

--- src/test/java/dev/playerblair/catalogingapp/manga/repository/AuthorRepositoryTest.java
+++ src/test/java/dev/playerblair/catalogingapp/manga/repository/AuthorRepositoryTest.java
@@ -3,16 +3,13 @@ package dev.playerblair.catalogingapp.manga.repository;
 import dev.playerblair.catalogingapp.manga.model.Author;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,24 +30,21 @@ public class AuthorRepositoryTest {
     @Autowired
     private AuthorRepository authorRepository;
 
-    private List<Author> authors;
-
     @BeforeEach
     public void setUp() {
         authorRepository.deleteAll();
 
         Author author1 = Author.builder()
                 .malId(1L)
-                .name("Author1")
+                .name("Test Author 1")
                 .build();
 
         Author author2 = Author.builder()
                 .malId(2L)
-                .name("Author2")
+                .name("Test Author 2")
                 .build();
 
-        authors = List.of(author1, author2);
-        authors.forEach(author -> authorRepository.save(author));
+        authorRepository.saveAll(List.of(author1, author2));
     }
 
     @Test
