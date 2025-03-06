972fd8521e4730945deb12c5d7664f22b49d647b
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-03-06 23:03:01 +0000

    add additional volume fields

--- src/main/java/dev/playerblair/catalogingapp/manga/dto/MangaCollectionUpdate.java
+++ src/main/java/dev/playerblair/catalogingapp/manga/dto/MangaCollectionUpdate.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MangaCollectionUpdate {
 
     private Long malId;
-    private boolean digitalCopy;
-    private int volumesOwned;
+    private boolean digitalCollection;
+    private boolean physicalCollection;
     private int volumesAvailable;
+    private int volumesOwned;
+    private List<Integer> volumesAcquired;
+    private String volumesEdition;
 }
