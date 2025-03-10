88697e5da770d9b75125d058def5b8e3ddccf5d9
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-03-10 18:08:48 +0000

    remove unused import

--- src/test/java/dev/playerblair/catalogingapp/api/service/ApiServiceTest.java
+++ src/test/java/dev/playerblair/catalogingapp/api/service/ApiServiceTest.java
@@ -8,7 +8,6 @@ import dev.playerblair.catalogingapp.manga.model.Author;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.ParameterizedTypeReference;
