cd88bdf7976b4e63f1b759112f52b917f701428f
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-03-11 00:41:44 +0000

    fix grammatical error in message

--- src/main/java/dev/playerblair/catalogingapp/manga/exception/MangaNotFoundException.java
+++ src/main/java/dev/playerblair/catalogingapp/manga/exception/MangaNotFoundException.java
@@ -2,6 +2,6 @@ package dev.playerblair.catalogingapp.manga.exception;
 
 public class MangaNotFoundException extends RuntimeException {
     public MangaNotFoundException(Long id) {
-        super("Manga with ID" + id + "not found.");
+        super("Manga with ID " + id + " not found.");
     }
 }
